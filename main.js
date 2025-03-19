58409ca63a6cd28d0191a4b3fd9540b60a08d57b
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-03-19 09:28:02 +0100

    pas fini

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ const buttonAcheter = document.querySelectorAll(".acheter");
 const boxDivList = document.querySelectorAll(".box");
 
 function createButtonBox() {
-	const buttonMoins = document.createElement("button");
-	const buttonPlus = document.createElement("button");
+	const buttonAcheter = document.createElement("button");
+	const imgAcheter = document.createElement("img");
+	const pAjouter = document.createElement("p");
+
+	buttonAcheter.classList.add("acheter");
+	pAjouter.classList.add("ajouter");
+
+	imgAcheter.src = "./Img/acheter.svg";
+	imgAcheter.alt = "acheter";
+
+	buttonAcheter.appendChild(imgAcheter);
+	buttonAcheter.appendChild(pAjouter);
+
+	return buttonAcheter;
 }
 
 function createButton() {
@@ -15,8 +27,8 @@ function createButton() {
 	let counter = 1;
 
 	divElement.classList.add("nombre");
-	buttonMoins.classList.add("choix");
-	buttonPlus.classList.add("choix", "pading");
+	buttonMoins.classList.add("choix", "pading");
+	buttonPlus.classList.add("choix");
 
 	spanELement.textContent = "1";
 	buttonMoins.textContent = "-";
@@ -30,7 +42,10 @@ function createButton() {
 		counter--;
 
 		if (counter <= 0) {
-			divElement.replaceWith();
+			const btn = createButton();
+			const btnBox = createButtonBox();
+
+			divElement.replaceWith(btnBox, btn);
 		}
 
 		spanELement.textContent = counter;
